f9fc81c9a95b216b7acf2805f2427efd8e043b9c
Willian Amaro Gomes <willianamaroti@gmail.com>
2021-11-19 11:22:26 -0300

    add js effects

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let slides = document.querySelectorAll(".slides");
+let dots = document.querySelectorAll(".dot");
+
+function setActive(i) {
+  for (slide of slides) {
+    slide.classList.remove("active");
+    slides[i].classList.add("active");
+  }
+  for (dot of dots) {
+    dot.classList.remove("active");
+    dots[i].classList.add("active");
+  }
+}
+
+for (let j = 0; j < dots.length; j++) {
+  dots[j].addEventListener("click", function () {
+    setActive(j);
+  });
+}
